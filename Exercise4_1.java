3c609332a4bb5035b0817e7da5c192ab1d88a6f2
Miraç Duran <mduran2341@gmail.com>
2025-12-04 18:43:49 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,25 @@
 public class Exercise4_1 {
 
     public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+        // American format: Day, Month Date, Year
+        System.out.println(day + ", " + month + " " + date + ", " + year);
     }
 
     public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+        // European format: Day Date Month Year
+        System.out.println(day + " " + date + " " + month + " " + year);
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+        String day = "Monday";
+        int date = 22;
+        String month = "July";
+        int year = 2019;
+
+        System.out.println("American format:");
+        printAmerican(day, date, month, year);
+
+        System.out.println("European format:");
+        printEuropean(day, date, month, year);
     }
 }
