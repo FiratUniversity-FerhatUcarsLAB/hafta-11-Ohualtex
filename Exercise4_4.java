41f0a1d2cad5ad180135f4719fd848ba550e7b21
Miraç Duran <mduran2341@gmail.com>
2025-12-04 19:25:00 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -12,9 +12,13 @@ public class Exercise4_4 {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
         getNumber();
+        /* Bu satir derlenir ve calisir. Metod 42 degerini dondurur ama bu deger hicbir yerde kullanilmaz. */
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
+        
+        /* Bu satir DERLENMEZ. void bir metod herhangi bir deger dondurmez,
+        bu nedenle sayHello() + 7 gibi bir ifadenin icinde kullanilamaz. */
 
         // Cevaplarinizi yorum olarak ekleyin.
     }
